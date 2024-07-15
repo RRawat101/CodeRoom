8c27ee9afe8ee4afd1b399d7d3fc44b61b5369ee
Rohit Rawat <rohit.rawat2504@gmail.com>
2024-07-15 20:27:41 +0530

    remove clone folder

--- clone/codeRoom/src/test.js
+++
@@ -1,6 +0,0 @@
-const  { loadLanguage, langNames, langs }= require( '@uiw/codemirror-extensions-langs');
-
-loadLanguage('tsx');
-langs.tsx();
-
-console.log('langNames:', langNames); // => "jsx" | "typescript" | "javascript" | "tsx"
